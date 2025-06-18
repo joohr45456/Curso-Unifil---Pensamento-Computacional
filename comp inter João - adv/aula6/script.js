d3addaef74f849d57989bf0a7d64600a7ae2b294
joohr45456 <91323903+joohr45456@users.noreply.github.com>
2025-06-18 17:13:43 -0300

    Começando com JS

---
+++ comp inter João - adv/aula6/script.js
@@ -0,0 +1,26 @@
+let nome = "Luiz";
+let sobrenome = "Fernando";
+
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+let nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);
+
+let teste = nome.toUpperCase();
+
+
+console.log(`Tamanho da string é ${nomeCompleto.length}`);
+console.log(`Tudo maiúsculo: ${nomeCompleto.toUpperCase()} `);
+console.log(`Tudo minúsculo: ${nomeCompleto.toLowerCase()}`);
+
+let string = "João Pedro";
+let number = 1234;
+let boolean = true;
+
+console.log(string)
+console.log(number)
+console.log(boolean)
+
+console.log(typeof (string) )
+console.log(typeof (number) )
+console.log(typeof (boolean) )
